fb565e45cb314ef25716cc23e5048bff9a46ffb6
Chris Laughlin <chris_laughlin@rapid7.com>
2015-11-22 13:21:46 +0000

    init commit

---
+++ src/js/module.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from 'components/main'
+require('./../scss/main.scss');
+ReactDOM.render(<Main />, document.getElementById('content'));
